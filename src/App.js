dd2ca7ea89bfdfa4b67433a2b4cbf55141707dec
Chris Vasquez <chrvasq@outlook.com>
2019-10-29 23:11:00 -0700

    Add form component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from "react";
+import ToDoForm from "./components/TodoComponents/TodoForm";
 
-class App extends React.Component {
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  addToDo = task => {
+    console.log("Added ToDo: ", task);
+    this.setState({
+      data: [
+        ...this.state.data,
+        { task: task, id: Date.now(), completed: false }
+      ]
+    });
+  };
+
+  // toggleCompleted = task => {
+  //   console.log('Completed: ', task);
+
+  // }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoForm addToDo={this.addToDo} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class ToDoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      task: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.task !== "") {
+      this.props.addToDo(this.state.task);
+      this.setState({ task: "" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="task"
+            value={this.state.task}
+            onChange={this.handleChange}
+            placeholder="Enter in a task"
+          />
+          <button>Add</button>
+        </form>
+        <button>Clear Completed</button>
+      </div>
+    );
+  }
+}
+
+export default ToDoForm;
